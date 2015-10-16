f2e1e53e8380712181a09a3edc7c3243aff29834
FanistyaDiasAfrilia <fanistyadiasafrilia@gmail.com>
2015-10-16 09:02:37 -0700

    1431140098

---
+++ New folder/Perulangan/Bintang.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Bintang
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lbl_pola = New System.Windows.Forms.Label()
+        Me.Lbl_baris = New System.Windows.Forms.Label()
+        Me.textboxbaris = New System.Windows.Forms.TextBox()
+        Me.buttonhasil = New System.Windows.Forms.Button()
+        Me.textboxhasil = New System.Windows.Forms.TextBox()
+        Me.combopola = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Lbl_pola
+        '
+        Me.Lbl_pola.AutoSize = True
+        Me.Lbl_pola.Location = New System.Drawing.Point(13, 19)
+        Me.Lbl_pola.Name = "Lbl_pola"
+        Me.Lbl_pola.Size = New System.Drawing.Size(34, 13)
+        Me.Lbl_pola.TabIndex = 1
+        Me.Lbl_pola.Text = "Pola :"
+        '
+        'Lbl_baris
+        '
+        Me.Lbl_baris.AutoSize = True
+        Me.Lbl_baris.Location = New System.Drawing.Point(13, 65)
+        Me.Lbl_baris.Name = "Lbl_baris"
+        Me.Lbl_baris.Size = New System.Drawing.Size(36, 13)
+        Me.Lbl_baris.TabIndex = 2
+        Me.Lbl_baris.Text = "Baris :"
+        '
+        'textboxbaris
+        '
+        Me.textboxbaris.Location = New System.Drawing.Point(53, 62)
+        Me.textboxbaris.Name = "textboxbaris"
+        Me.textboxbaris.Size = New System.Drawing.Size(266, 20)
+        Me.textboxbaris.TabIndex = 3
+        '
+        'buttonhasil
+        '
+        Me.buttonhasil.Location = New System.Drawing.Point(325, 60)
+        Me.buttonhasil.Name = "buttonhasil"
+        Me.buttonhasil.Size = New System.Drawing.Size(114, 23)
+        Me.buttonhasil.TabIndex = 4
+        Me.buttonhasil.Text = "hasil"
+        Me.buttonhasil.UseVisualStyleBackColor = True
+        '
+        'textboxhasil
+        '
+        Me.textboxhasil.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.textboxhasil.Location = New System.Drawing.Point(16, 108)
+        Me.textboxhasil.Multiline = True
+        Me.textboxhasil.Name = "textboxhasil"
+        Me.textboxhasil.Size = New System.Drawing.Size(423, 305)
+        Me.textboxhasil.TabIndex = 5
+        '
+        'combopola
+        '
+        Me.combopola.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.combopola.FormattingEnabled = True
+        Me.combopola.Items.AddRange(New Object() {"Pyramid", "Hollow Pyramid", "Inverted Pyramid", "Hollow Inverted Pyramid"})
+        Me.combopola.Location = New System.Drawing.Point(53, 15)
+        Me.combopola.Name = "combopola"
+        Me.combopola.Size = New System.Drawing.Size(386, 21)
+        Me.combopola.TabIndex = 2
+        '
+        'Bintang
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(465, 425)
+        Me.Controls.Add(Me.combopola)
+        Me.Controls.Add(Me.textboxhasil)
+        Me.Controls.Add(Me.buttonhasil)
+        Me.Controls.Add(Me.textboxbaris)
+        Me.Controls.Add(Me.Lbl_baris)
+        Me.Controls.Add(Me.Lbl_pola)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Bintang"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Problem Bintang"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Lbl_pola As System.Windows.Forms.Label
+    Friend WithEvents Lbl_baris As System.Windows.Forms.Label
+    Friend WithEvents textboxbaris As System.Windows.Forms.TextBox
+    Friend WithEvents buttonhasil As System.Windows.Forms.Button
+    Friend WithEvents textboxhasil As System.Windows.Forms.TextBox
+    Friend WithEvents combopola As System.Windows.Forms.ComboBox
+End Class
---
+++ New folder/Perulangan/Matriks.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Matriks
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lbl_matrik1 = New System.Windows.Forms.Label()
+        Me.Txb_matrik1 = New System.Windows.Forms.TextBox()
+        Me.Lbl_operasi = New System.Windows.Forms.Label()
+        Me.Cmb_operasi = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.textboxmatrik2 = New System.Windows.Forms.TextBox()
+        Me.textboxhasil = New System.Windows.Forms.TextBox()
+        Me.Lbl_hasil = New System.Windows.Forms.Label()
+        Me.buttonhasil = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Lbl_matrik1
+        '
+        Me.Lbl_matrik1.AutoSize = True
+        Me.Lbl_matrik1.Location = New System.Drawing.Point(13, 23)
+        Me.Lbl_matrik1.Name = "Lbl_matrik1"
+        Me.Lbl_matrik1.Size = New System.Drawing.Size(42, 13)
+        Me.Lbl_matrik1.TabIndex = 0
+        Me.Lbl_matrik1.Text = "Matrik1"
+        '
+        'Txb_matrik1
+        '
+        Me.Txb_matrik1.Location = New System.Drawing.Point(61, 20)
+        Me.Txb_matrik1.Multiline = True
+        Me.Txb_matrik1.Name = "Txb_matrik1"
+        Me.Txb_matrik1.Size = New System.Drawing.Size(368, 76)
+        Me.Txb_matrik1.TabIndex = 1
+        '
+        'Lbl_operasi
+        '
+        Me.Lbl_operasi.AutoSize = True
+        Me.Lbl_operasi.Location = New System.Drawing.Point(12, 121)
+        Me.Lbl_operasi.Name = "Lbl_operasi"
+        Me.Lbl_operasi.Size = New System.Drawing.Size(43, 13)
+        Me.Lbl_operasi.TabIndex = 2
+        Me.Lbl_operasi.Text = "Operasi"
+        '
+        'Cmb_operasi
+        '
+        Me.Cmb_operasi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Cmb_operasi.FormattingEnabled = True
+        Me.Cmb_operasi.Items.AddRange(New Object() {"Kurang", "Tambah"})
+        Me.Cmb_operasi.Location = New System.Drawing.Point(61, 118)
+        Me.Cmb_operasi.Name = "Cmb_operasi"
+        Me.Cmb_operasi.Size = New System.Drawing.Size(368, 21)
+        Me.Cmb_operasi.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 168)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Matrik 2"
+        '
+        'textboxmatrik2
+        '
+        Me.textboxmatrik2.Location = New System.Drawing.Point(61, 165)
+        Me.textboxmatrik2.Multiline = True
+        Me.textboxmatrik2.Name = "textboxmatrik2"
+        Me.textboxmatrik2.Size = New System.Drawing.Size(368, 76)
+        Me.textboxmatrik2.TabIndex = 5
+        '
+        'textboxhasil
+        '
+        Me.textboxhasil.Location = New System.Drawing.Point(61, 260)
+        Me.textboxhasil.Multiline = True
+        Me.textboxhasil.Name = "textboxhasil"
+        Me.textboxhasil.Size = New System.Drawing.Size(368, 76)
+        Me.textboxhasil.TabIndex = 6
+        '
+        'Lbl_hasil
+        '
+        Me.Lbl_hasil.AutoSize = True
+        Me.Lbl_hasil.Location = New System.Drawing.Point(12, 263)
+        Me.Lbl_hasil.Name = "Lbl_hasil"
+        Me.Lbl_hasil.Size = New System.Drawing.Size(30, 13)
+        Me.Lbl_hasil.TabIndex = 7
+        Me.Lbl_hasil.Text = "Hasil"
+        '
+        'buttonhasil
+        '
+        Me.buttonhasil.Location = New System.Drawing.Point(307, 351)
+        Me.buttonhasil.Name = "buttonhasil"
+        Me.buttonhasil.Size = New System.Drawing.Size(122, 23)
+        Me.buttonhasil.TabIndex = 8
+        Me.buttonhasil.Text = "Hasil"
+        Me.buttonhasil.UseVisualStyleBackColor = True
+        '
+        'Matriks
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(441, 402)
+        Me.Controls.Add(Me.buttonhasil)
+        Me.Controls.Add(Me.Lbl_hasil)
+        Me.Controls.Add(Me.textboxhasil)
+        Me.Controls.Add(Me.textboxmatrik2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Cmb_operasi)
+        Me.Controls.Add(Me.Lbl_operasi)
+        Me.Controls.Add(Me.Txb_matrik1)
+        Me.Controls.Add(Me.Lbl_matrik1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Matriks"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Matriks Kalkulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Lbl_matrik1 As System.Windows.Forms.Label
+    Friend WithEvents Txb_matrik1 As System.Windows.Forms.TextBox
+    Friend WithEvents Lbl_operasi As System.Windows.Forms.Label
+    Friend WithEvents Cmb_operasi As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents textboxmatrik2 As System.Windows.Forms.TextBox
+    Friend WithEvents textboxhasil As System.Windows.Forms.TextBox
+    Friend WithEvents Lbl_hasil As System.Windows.Forms.Label
+    Friend WithEvents buttonhasil As System.Windows.Forms.Button
+End Class
